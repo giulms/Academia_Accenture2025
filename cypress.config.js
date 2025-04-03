674d33d4a80994fa4de19c5051901d25e39b5790
giulms <expertgiulliano@gmail.com>
2025-04-03 03:58:37 -0300

    Configuração do ambiente

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    watchForFileChanges: false,
+    specPattern: 'cypress/e2e/**/*.feature',
+    baseUrl: 'https://sampleapp.tricentis.com/101/app.php',
+
+    setupNodeEvents(on, config) {
+      const cucumber = require('cypress-cucumber-preprocessor').default;
+      on('file:preprocessor', cucumber());
+    },
+  },
+});
---
+++ cypress/e2e/preencherAbas-01.feature
@@ -0,0 +1,5 @@
+#utf-8
+#language: pt
+
+Funcionalidade: preencherAbas-01 - Adição de informações válidas nas páginas
+    Cenário: Validação do preenchimento das abas
